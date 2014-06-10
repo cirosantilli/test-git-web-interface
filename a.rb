cc6ef4d60b51406d338040a28c61bcd7b93bd48f
Ciro Santilli <ciro.santilli@gmail.com>
2014-06-10 16:12:47 +0200

    chaining

--- a.rb
+++ a.rb
@@ -1,3 +1,32 @@
 def f(x)
-  return x + 1
+  x + 1
 end
+
+def g(x)
+  x + 1
+end
+
+def h(x)
+  x + 1
+end
+
+def a
+  1
+end
+
+def b
+  1
+end
+
+def c
+  1
+end
+
+Chaining style:
+
+a.
+  b.
+  c
+
+a.b.
+  c
