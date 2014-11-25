2d03cf60104e9a3578835aacdf0417d703216678
Ciro Santilli <ciro.santilli@gmail.com>
2014-11-25 11:51:02 +0100

    Remotes variable repo.

--- remotes.sh
+++ remotes.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
-git remote add at      git@atlas-tmp:tmp/test.git
-git remote add as      git@git.assembla.com:cirosantilli-test.git
-git remote add bb      git@bitbucket.org:cirosantilli/test.git
-git remote add gc      https://ciro.santilli:gv9Uf5kT7Ds5@code.google.com/p/cirosantilli-test
-git remote add gh      git@github.com:cirosantilli/test.git
-git remote add gl      git@gitlab.com:cirosantilli/test.git
-git remote add gll     http://root:5iveL!fe@localhost:3000/gitlab-org/gitlab-test.git
-git remote add origin  git@github.com:cirosantilli/test.git
-git remote add pf      git@www.penflip.com:cirosantilli/test.git
-git remote add sf      ssh://cirosantilli@git.code.sf.net/p/cirosantilli-test/code
+
+if [ $# -gt 0 ]; then
+  REPO="$1"
+  shift
+else
+  REPO=test
+fi
+
+git remote add at      git@atlas-tmp:tmp/${REPO}.git
+git remote add as      git@git.assembla.com:cirosantilli-${REPO}.git
+git remote add bb      git@bitbucket.org:cirosantilli/${REPO}.git
+git remote add gc      https://ciro.santilli:gv9Uf5kT7Ds5@code.google.com/p/cirosantilli-${REPO}
+git remote add gh      git@github.com:cirosantilli/${REPO}.git
+git remote add gl      git@gitlab.com:cirosantilli/${REPO}.git
+git remote add gll     http://root:5iveL!fe@localhost:3000/gitlab-org/gitlab-${REPO}.git
+git remote add origin  git@github.com:cirosantilli/${REPO}.git
+git remote add pf      git@www.penflip.com:cirosantilli/${REPO}.git
+git remote add sf      ssh://cirosantilli@git.code.sf.net/p/cirosantilli-${REPO}/code
